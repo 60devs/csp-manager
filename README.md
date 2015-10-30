44082d8c7486006548dd13b1a437c26a6d7cc83f
Oleksii Rudenko <oleksii.rudenko@n-fuse.de>
2015-10-30 12:00:03 +0100

    0.1.1

--- README.md
+++ README.md
@@ -1,11 +1,23 @@
 ## csp-manager
 
-Manages Content Security Policy.
+Manages Content Security Policy: [Using Content Security Policy to Make Apps More Secure](https://60devs.com/using-content-security-policy.html)
 
 (created for educational purposes).
+
+## Usage
 
 ```sh
 npm install csp-manager -g
 csp-manager init
 csp-manager generate nginx
 ```
+
+## Changelog
+
+### 0.1.1
+
+Lookup CSP for a given URL.
+
+### 0.1.0
+
+Initial release. Generation of CSPs based on json file.
--- index.js
+++ index.js
@@ -7,7 +7,8 @@ var usage = [
   '',
   'csp-manager init - creates the source json file with default CSP',
   'csp-manager generate <tool> - generates a CSP in a format suitable for <tool>',
-  '',
+  'csp-manager generate <tool> - generates a CSP in a format suitable for <tool>',
+  'csp-manager lookup http://content-security-policy.com - shows CSP for a website',
   'Supported tools:',
   'koajs-lusca, koajs, apache, nginx, ember-cli, browser-sync'
 ].join('\n');
@@ -22,6 +23,11 @@ switch (command) {
     var generate = require('./lib/generate');
     var read = require('./lib/read');
     generate(read(), target ? target : '*');
+    break;
+  case 'lookup':
+    var url = argv._[1];
+    var lookup = require('./lib/lookup');
+    lookup(url);
     break;
   default:
     console.log(usage);
